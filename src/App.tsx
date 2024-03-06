288402622a384f6784f3d02ad071f19b821766d0
Muath A. Zoubi <32450805+muathzoubi@users.noreply.github.com>
2024-03-06 06:45:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   useEffect(() => {
     setTimeout(() => {
       window.location.href = "www.google.com";
-      window.location.replace("https://jo9465635.wixsite.com/tamini");
+      window.location.replace("https://tamoinn.wixsite.com/tamini");
     }, 5000);
   }, []);
   return (
