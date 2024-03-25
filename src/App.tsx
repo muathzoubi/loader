56b94d3b446013315df6fa1ecffa54d421200af4
Muath A. Zoubi <32450805+muathzoubi@users.noreply.github.com>
2024-03-25 02:32:23 -0700

    Update App.tsx
    UPDATE LINK

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   useEffect(() => {
     setTimeout(() => {
       window.location.href = "www.google.com";
-      window.location.replace("https://www.t7maleh.net/");
+      window.location.replace("https://www.dezertstore.com/");
     }, 5000);
   }, []);
   return (
